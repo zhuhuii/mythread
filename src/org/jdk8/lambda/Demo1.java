e7ba9d87d1914d584f7c4da1f60c96fb8a851bca
zhuhui <897762690@qq.com>
2020-05-25 23:17:26 +0800

    java.util.concurrent

---
+++ src/org/jdk8/lambda/Demo1.java
@@ -0,0 +1,15 @@
+package org.jdk8.lambda;
+
+/**
+ * JDK1.5
+ *  泛型、枚举、反射、注解
+ *
+ * JDK1.8
+ *  lambda表达式、链式编程、函数式接口、Stream流式计算
+ *
+ *  超级多FunctionalInterface
+ *  简化编程模型，在新版本的框架底层大量应用！
+ *  foreach(消费者类的函数式接口)
+ */
+public class Demo1 {
+}
